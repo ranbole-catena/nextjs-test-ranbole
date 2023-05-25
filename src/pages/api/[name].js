3451e0739e606f57bdf78d8f65ed28b90f3c9552
Ranbo Le <ranbo.le@catenamedia.com>
2023-05-25 14:59:02 -0400

    cleanup

--- src/pages/api/[name].js
+++ src/pages/api/[name].js
@@ -1,6 +1,3 @@
 export default function handler(request, response) {
-    //console.log(request.query);
-    // return response.end('here');
-    // const { name } = request.query;
     return response.end(`Hello ${request.query.name}!`);
 }
--- src/pages/api/user/[id].js
+++ src/pages/api/user/[id].js
@@ -2,9 +2,7 @@ import mysql from 'mysql2/promise';
 
 export default async function handler(request, response) {
     const userId = request.query.id;
-    //const mysql = require('mysql2')
     const connection = await mysql.createConnection(process.env.DATABASE_URL)
-    console.log('DB URL', process.env.DATABASE_URL);
 
     const [rows, fields] = await connection.execute('SELECT * FROM `users` WHERE `id` = ?', [userId])
 
@@ -15,9 +13,4 @@ export default async function handler(request, response) {
         // cookies: request.cookies,
         // env: process.env.MY_SECRET,
     });
-
-
-    // let user = rows[0]
-    console.log('user', user)
-    connection.end()
 }
