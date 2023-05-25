32b059a1938dc7e24af91295179c447a50a2dda5
Ranbo Le <ranbo.le@catenamedia.com>
2023-05-25 18:28:34 -0400

    fix

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -2,37 +2,36 @@
 import Link from "next/link";
 
 export default async function Page() {
-    return null;
-    // const users = await getData();
-    // return (
-    //     <div className="p-5">
-    //         <h1 className="text-xl mb-5">This is a user directory page</h1>
-    //         <table className="table-auto w-1/2">
-    //             <thead>
-    //                 <tr>
-    //                     <th className="text-left">ID</th>
-    //                     <th className="text-left">Name</th>
-    //                     <th className="text-left">Email</th>
-    //                     <th className="text-left"></th>
-    //                 </tr>
-    //             </thead>
-    //             <tbody>
-    //                 {
-    //                     users.map((user, i) => {
-    //                         return (
-    //                             <tr key={i}>
-    //                                 <td>{user.id}</td>
-    //                                 <td>{user.name}</td>
-    //                                 <td>{user.email}</td>
-    //                                 <td><Link className="text-orange-300" href={`/user/${user.id}`}>view</Link></td>
-    //                             </tr>
-    //                         )
-    //                     })
-    //                 }
-    //             </tbody>
-    //         </table>
-    //     </div>
-    // )
+    const users = await getData();
+    return (
+        <div className="p-5">
+            <h1 className="text-xl mb-5">This is a user directory page</h1>
+            <table className="table-auto w-1/2">
+                <thead>
+                    <tr>
+                        <th className="text-left">ID</th>
+                        <th className="text-left">Name</th>
+                        <th className="text-left">Email</th>
+                        <th className="text-left"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td><Link className="text-orange-300" href={`/user/${user.id}`}>view</Link></td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 async function getData() {
