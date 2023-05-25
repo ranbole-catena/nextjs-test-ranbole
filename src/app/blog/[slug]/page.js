d28953393dc2eb1e905960159c32d6d2e548a31f
Ranbo Le <ranbo.le@catenamedia.com>
2023-05-25 17:00:28 -0400

    update

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,4 +1,24 @@
-export default function Page({ params }) {
+import Link from "next/link";
 
-    return <p>Post: {params.slug}</p>;
+export default async function Page({ params }) {
+    const data = await getData(params.slug);
+    //console.log('data', data)
+    return <>
+        <p>Post, user name from API is: {data.user.name}</p>
+        <Link href={'/blog'}>link to blog dir</Link>
+    </>
+}
+
+async function getData(userId) {
+    const res = await fetch(`http://localhost:3000/api/user/${userId}`);
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    // Recommendation: handle errors
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
 }
