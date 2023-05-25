71ea08c220169d20a21f6cc70c012491917099f6
Ranbo Le <ranbo.le@catenamedia.com>
2023-05-25 18:14:57 -0400

    fix

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function Home() {
+  return (
+    <main className='text-center text-4xl mt-5'>
+      <div>
+        Current Env: {`${process.env.ENV}`}
+      </div>
+      <div>
+        <Link href={`/user`} className="text-orange-300 text-4xl">View Users</Link>
+      </div>
+    </main>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main>
-      Ranbo testing pushed a change from a ticket
-    </main>
-  )
-}
