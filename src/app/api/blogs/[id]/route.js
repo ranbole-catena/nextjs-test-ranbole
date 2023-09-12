0083bec530bc773d06dfba63fa733bc6b93187d4
Ranbo Le <ranbo.le@catenamedia.com>
2023-09-11 22:45:25 -0400

    redis

--- src/app/api/blogs/[id]/route.js
+++ src/app/api/blogs/[id]/route.js
@@ -1,25 +1,33 @@
 import { NextResponse } from "next/server";
 import { connect } from "@planetscale/database";
+import { kv } from "@vercel/kv";
 
 export async function GET(request, { params }) {
   console.log("request", request);
   const id = params.id;
 
-  const config = {
-    url: process.env["DATABASE_URL"],
-  };
+  const kv_key = `blog_${id}`;
+  let data = await kv.get(kv_key);
 
-  const conn = await connect(config);
+  if (!data) {
+    const config = {
+      url: process.env["DATABASE_URL"],
+    };
 
-  const results = await conn.execute("SELECT * FROM users where ID = ?", [id]);
+    const conn = await connect(config);
+
+    const results = await conn.execute("SELECT * FROM users where ID = ?", [
+      id,
+    ]);
+    data = results.rows[0] ?? {};
+    await kv.set(kv_key, data);
+  }
 
   // const res = await fetch('https://data.mongodb-api.com/...', {
   //   next: { revalidate: 60 }, // Revalidate every 60 seconds
   // })
   // const data = await res.json()
-
-  console.log("results", results);
   // return NextResponse.json(data)
   //return new Response.json("here");
-  return NextResponse.json(results.rows[0] ?? {});
+  return NextResponse.json(data);
 }
--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,24 +1,33 @@
 import { NextResponse } from "next/server";
 import { connect } from "@planetscale/database";
+import { kv } from "@vercel/kv";
 
 export async function GET(request) {
   console.log("request", request);
 
-  const config = {
-    url: process.env["DATABASE_URL"],
-  };
+  // check redis first
+  const kv_key = "blog_list";
+  let data = await kv.get(kv_key);
 
-  const conn = await connect(config);
+  if (!data) {
+    const config = {
+      url: process.env["DATABASE_URL"],
+    };
 
-  const results = await conn.execute("SELECT * FROM users");
+    const conn = await connect(config);
+
+    const results = await conn.execute("SELECT * FROM users");
+    data = results.rows;
+    await kv.set(kv_key, data);
+  }
 
   // const res = await fetch('https://data.mongodb-api.com/...', {
   //   next: { revalidate: 60 }, // Revalidate every 60 seconds
   // })
   // const data = await res.json()
 
-  console.log("results", results);
+  //console.log("results", results);
   // return NextResponse.json(data)
   //return new Response.json("here");
-  return NextResponse.json(results.rows);
+  return NextResponse.json(data);
 }
