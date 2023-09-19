994daa7b7dc01c645c8ba5f56acba36de5625a11
Ranbo Le <ranbo.le@catenamedia.com>
2023-09-19 12:47:12 -0400

    api update

--- src/app/api/blogs/[id]/route.js
+++ src/app/api/blogs/[id]/route.js
@@ -3,13 +3,13 @@ import { connect } from "@planetscale/database";
 import { kv } from "@vercel/kv";
 
 export async function GET(request, { params }) {
-  console.log("request", request);
+  //console.log("request", request);
   const id = params.id;
 
   const kv_key = `blog_${id}`;
-  let data = await kv.get(kv_key);
-
-  if (!data) {
+  //let data = await kv.get(kv_key);
+  let data = {};
+  if (true) {
     const config = {
       url: process.env["DATABASE_URL"],
     };
@@ -23,8 +23,19 @@ export async function GET(request, { params }) {
 
     console.log("results", results);
     data = results.rows[0] ?? {};
-    await kv.set(kv_key, data, { ex: 3600, nx: true });
+    await kv.set(kv_key, data, { ex: 3600, nx: false }); // nx = only set if it doesn't exists
   }
+
+  let categories = [];
+  let categories_arr = data.categories.split(",");
+  let categoriy_slug_arr = data.category_slugs.split(",");
+
+  categories_arr.forEach((c, i) => {
+    categories.push({
+      name: c,
+      slug: categoriy_slug_arr[i],
+    });
+  });
 
   // const res = await fetch('https://data.mongodb-api.com/...', {
   //   next: { revalidate: 60 }, // Revalidate every 60 seconds
@@ -32,5 +43,18 @@ export async function GET(request, { params }) {
   // const data = await res.json()
   // return NextResponse.json(data)
   //return new Response.json("here");
-  return NextResponse.json(data);
+
+  return NextResponse.json({
+    title: data.title,
+    categories: categories,
+    author: {
+      name: data.author_name,
+      img: "https://www.playusa.com/wp-content/uploads/2020/03/4eeb614417c557bad75cbc00fcfff3f9.jpeg",
+      bio: "Derek Helling is the assistant managing editor of PlayUSA. Helling focuses on breaking news, including finance, regulation, and technology in the gaming industry. Helling completed his journalism degree at the University of Iowa and resides in Chicago",
+    },
+    date: data.published_on,
+    banner_img:
+      "https://www.playusa.com/wp-content/uploads/2023/08/racing-louisville-soccer-match-nwsl-1024x445.jpeg",
+    content: data.body,
+  });
 }
